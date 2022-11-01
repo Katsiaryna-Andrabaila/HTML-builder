ed2320b02b1106e78cbfa98e92f96affbff78ed9
katsiaryna-andrabaila <androbailoe@gmail.com>
2022-11-01 23:28:08 +0300

    feat: implement 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+const folder = path.join(__dirname, 'secret-folder');
+
+fs.readdir(folder, {withFileTypes: true}, (error, files) => {
+    if (error) throw error;
+    for (let i = 0; i < files.length; i++) {
+        let currentFile = path.join(folder, files[i].name);
+        fs.stat(currentFile, (statError, Dirent) => {
+            if(statError) throw statError;
+            if (files[i].isFile()) {
+                console.log(`${files[i].name.slice(0, files[i].name.indexOf('.'))} - ${path.extname(files[i].name).slice(1)} - ${Dirent.size / 1000}kb`);
+            }
+        })
+    }
+});
