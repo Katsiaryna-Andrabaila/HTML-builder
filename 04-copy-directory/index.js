c1b701a7c4d837383d20f7195a479fe263588bc4
katsiaryna-andrabaila <androbailoe@gmail.com>
2022-11-02 21:57:28 +0300

    feat: implement 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,3 +1,35 @@
 const fs = require('fs');
 const path = require('path');
 const folder = path.join(__dirname, 'files');
+const newFolder = path.join(__dirname, 'files-copy');
+
+fs.access(newFolder, (err) => {
+    if (err) {
+        fs.mkdir(newFolder, error => {
+            if(error) throw error;
+            copyFolder();
+        });
+    } else {
+        fs.readdir(newFolder, (error, files) => {
+            if (error) throw error;
+            for (const file of files) {
+                fs.unlink(path.join(newFolder, file), error => {
+                    if(error) throw error;
+                    copyFolder();
+                });
+            }
+        });
+    }
+});
+
+function copyFolder() {
+    fs.readdir(folder, (error, files) => {
+        if(error) throw error;
+        for (const file of files) {
+            fs.copyFile(path.join(folder, file), path.join(newFolder, file), error => {
+                if(error) throw error;
+            });
+        }
+    });
+}
+
