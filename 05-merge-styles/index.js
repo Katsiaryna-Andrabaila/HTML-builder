8578ea7ab6c0748283169ccbc1706236800de0e5
katsiaryna-andrabaila <androbailoe@gmail.com>
2022-11-03 15:54:29 +0300

    feat: implement 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const folder = path.join(__dirname, 'styles');
+const targetFolder = path.join(__dirname, 'project-dist');
+const bundle = path.join(targetFolder, 'bundle.css');
+
+const onStream = fs.createWriteStream(bundle, 'utf-8');
+
+fs.readdir(folder, (error, files) => {
+    if (error) throw error;
+    for (const file of files) {
+        if(path.extname(file) === '.css') {
+            const outStream = fs.createReadStream(path.join(folder, file), 'utf-8');
+            outStream.on('data', chunk => {
+                onStream.write(chunk);
+            })
+        }
+    }
+})
